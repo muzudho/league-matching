e782114c648c839015cca2cbf78ebe1c2d5ce405
むずでょ <muzudho1@gmail.com>
2023-12-17 21:20:48 +0900

    create board

--- step_2_0.py
+++ step_2_0.py
@@ -2,6 +2,35 @@
 # python step_2_0.py
 #
 
+
+def create_empty_board(edge_size):
+    """辺の長さを指定して、盤を生成
+    
+    Parameters
+    ----------
+    edge_size : int
+        一辺の長さ
+    """
+    return [" "] * (edge_size * edge_size)
+
+
+def print_board(edge_size, board):
+    """盤の表示
+    
+    Parameters
+    ----------
+    edge_size : int
+        一辺の長さ
+    board : list
+        盤
+    """
+    for y in range(0,edge_size):
+        for x in range(0,edge_size):
+            print(board[y*edge_size+x], end="")
+        print("")
+    pass
+
+
 if __name__ == "__main__":
 
     print("Start. Please enter key.")
@@ -12,7 +41,7 @@ if __name__ == "__main__":
 
     # 辺の長さを指定して、盤を生成
     edge_size = 1
-    board = [" "] * (edge_size * edge_size)
+    board = create_empty_board(edge_size)
 
     # 対角線をプロット
     for y in range(0,edge_size):
@@ -21,17 +50,14 @@ if __name__ == "__main__":
                 board[y*edge_size+x] = "\\"
 
     # 表示
-    for y in range(0,edge_size):
-        for x in range(0,edge_size):
-            print(board[y*edge_size+x], end="")
-        print("")
+    print_board(edge_size, board)
 
     print("Please enter key.")
     input()
 
     # 辺の長さを指定して、盤を生成
     edge_size = 2
-    board2 = [" "] * (edge_size * edge_size)
+    board2 = create_empty_board(edge_size)
 
     # 対角線をプロット
     for y in range(0,edge_size):
@@ -40,9 +66,6 @@ if __name__ == "__main__":
                 board2[y*edge_size+x] = "\\"
 
     # 表示
-    for y in range(0,edge_size):
-        for x in range(0,edge_size):
-            print(board2[y*edge_size+x], end="")
-        print("")
+    print_board(edge_size, board2)
 
     print("Finished.")
