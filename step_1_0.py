af8dbcb65deb23b5195e555441f88c0f53545a29
むずでょ <muzudho1@gmail.com>
2023-12-17 14:23:38 +0900

    diagonal

--- step_1_0.py
+++ step_1_0.py
@@ -2,16 +2,44 @@
 # python step_1_0.py
 #
 
+def get_opponent(me, round, player_size):
+    """対戦相手の選手番号（基数）を取得
+
+    Parameters
+    ----------
+    me : int
+        自分の選手番号（基数）
+    round : int
+        ラウンド数（序数）
+    player_size : int
+        選手全員の数
+    """
+    if me % 2 == 0:
+        return (me + player_size + round) % player_size
+    else:
+        return (me + player_size - round) % player_size
+
+
 if __name__ == "__main__":
+
+    print("hello")
+
+    # ラウンド番目
+    round = 1
 
     # 辺の長さ。正方形とする
     edge_size = 8
 
     # 8x8 の配列
-    board = ["*"] * (edge_size * edge_size)
+    board = [" "] * (edge_size * edge_size)
 
-    print("hello")
+    # 対角線をプロット
+    for y in range(0,edge_size):
+        for x in range(0,edge_size):
+            if y == x:
+                board[y*edge_size+x] = "\\"
 
+    # 表示
     for y in range(0,edge_size):
         for x in range(0,edge_size):
             print(board[y*edge_size+x], end="")
